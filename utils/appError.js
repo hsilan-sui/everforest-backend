82512b80a2f1ac8bdf98431e565fb91979f734e6
Helena Chen <cocolulu2327@gmail.com>
2025-04-05 16:50:47 +0800

    fix: 函式表達式

--- utils/appError.js
+++ utils/appError.js
@@ -1,9 +1,9 @@
 // 自訂錯誤類別
-function AppError(statusCode, message) {
+const AppError = (statusCode, message) => {
   const error = new Error(message);
   error.statusCode = statusCode;
   error.isOperational = true; // 用來標記這是應用層的錯誤
   return error;
-}
+};
 
 module.exports = AppError;
