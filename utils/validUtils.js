2e4cd72c4edb3dbefa75fca9d4768d8cfa3b3083
Helena Chen <96384004+ChenHelena@users.noreply.github.com>
2025-04-05 16:53:51 +0800

    fix: 函式表達式 (#11)
    - OK

--- utils/validUtils.js
+++ utils/validUtils.js
@@ -1,20 +1,20 @@
-function isUndefined(value) {
+const isUndefined = (value) => {
   return value === undefined;
-}
+};
 
-function isNotValidString(value) {
+const isNotValidString = (value) => {
   return typeof value !== "string" || value.trim().length === 0 || value === "";
-}
+};
 
-function isNotValidUUID(value) {
+const isNotValidUUID = (value) => {
   const uuidPattern =
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
   return !uuidPattern.test(value);
-}
+};
 
-function isNotValidInteger(value) {
+const isNotValidInteger = (value) => {
   return typeof value !== "number" || value < 0 || value % 1 !== 0;
-}
+};
 
 const isValidPassword = (value) => {
   const passwordPattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}/;
