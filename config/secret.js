ba7a2d7418d20c04076e48f0f16b8115c3dfa0ae
Helena Chen <cocolulu2327@gmail.com>
2025-05-30 16:20:00 +0800

    fix: secret.js & passport.js

--- config/secret.js
+++ config/secret.js
@@ -14,12 +14,4 @@ module.exports = {
         ? `${process.env.BACKEND_PRO_ORIGIN}/api/v1/auth/oauth/google/callback`
         : `${process.env.BACKEND_DEV_ORIGIN}/api/v1/auth/oauth/google/callback`,
   },
-  google: {
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:
-      process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_PRO_ORIGIN
-        : process.env.FRONTEND_DEV_ORIGIN,
-  },
 };
