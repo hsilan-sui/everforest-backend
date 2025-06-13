79274f4bcc01efc40bc1799811ba2b5764d0f22d
Helena <96384004+ChenHelena@users.noreply.github.com>
2025-06-13 17:17:39 +0800

    Update orderController.js
    更新導回前端頁面的網址

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -99,7 +99,7 @@ const orderController = {
           ? orders[0].eventPlanBox.title
           : orders.map((o) => o.eventPlanBox.title).join("#"),
       ReturnURL: `${process.env.BACKEND_DEV_ORIGIN}/api/v1/member/orders/payment-callback`,
-      ClientBackURL: `${process.env.FRONTEND_DEV_ORIGIN}/payment-success`,
+      ClientBackURL: `${process.env.FRONTEND_DEV_ORIGIN}/api/v1/member/orders/payment-success`,
       PaymentType: "aio",
       ChoosePayment: "ALL",
       EncryptType: 1,
