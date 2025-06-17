b236f3c14f2add7292a49766d5f9c1d5040e9c66
Helena <96384004+ChenHelena@users.noreply.github.com>
2025-06-17 21:20:09 +0800

    Update authController.js
    修正第三方登入

--- controllers/authController.js
+++ controllers/authController.js
@@ -348,6 +348,8 @@ const authController = {
       }
 
       const userPayload = {
+        id: member.id,
+        role: member.role,
         email,
         googleId,
         name,
@@ -379,8 +381,8 @@ const authController = {
       // 重定向到前端頁面
       const redirectURL =
         process.env.NODE_ENV === "production"
-          ? `${process.env.FRONTEND_PRO_ORIGIN}/?accessToken=${accessToken}&refreshToken=${refreshToken}`
-          : `${process.env.FRONTEND_DEV_ORIGIN}/?accessToken=${accessToken}&refreshToken=${refreshToken}`;
+          ? `${process.env.FRONTEND_PRO_ORIGIN}/?access_token=${accessToken}&refresh_token=${refreshToken}`
+          : `${process.env.FRONTEND_DEV_ORIGIN}/?access_token=${accessToken}&refresh_token=${refreshToken}`;
       res.redirect(redirectURL);
     } catch (error) {
       await queryRunner.rollbackTransaction(); // 回滾事務
